05855f2b6e9defdc6185729ac96b87b228705bc8
DESKTOP-7T5S3LD\HJ <hallongjj@naver.com>
2021-06-08 13:16:36 +0900

    네번째

--- src/main/java/edu/bit/ex/controller/SecurityController.java
+++ src/main/java/edu/bit/ex/controller/SecurityController.java
@@ -23,4 +23,10 @@ public class SecurityController {
       log.info("logined member");
    }
    
+   @GetMapping("/admin")
+   public void doAdmin() {
+
+      log.info("logined admin");
+   }
+   
 }
