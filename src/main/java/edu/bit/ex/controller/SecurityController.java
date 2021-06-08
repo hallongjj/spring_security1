751747d853114a905d7b8387bcfa4635b97a2cd1
DESKTOP-7T5S3LD\HJ <hallongjj@naver.com>
2021-06-08 12:14:12 +0900

    두번째

---
+++ src/main/java/edu/bit/ex/controller/SecurityController.java
@@ -0,0 +1,26 @@
+package edu.bit.ex.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import lombok.extern.log4j.Log4j;
+
+@Log4j
+@RequestMapping("/security/*")
+@Controller
+public class SecurityController {
+   
+    // /security/all.jsp을 불러옴.
+   @GetMapping("/all")
+   public void doAll() {
+      log.info("do all can access everybody");
+   }
+
+   @GetMapping("/member")
+   public void doMember() {
+
+      log.info("logined member");
+   }
+   
+}
